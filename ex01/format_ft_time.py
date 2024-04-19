6045927fd9e6d72baf107293931b61259d148f4b
ARTHUR BIED-CHARRETON <arthurbiedchar@gmail.com>
2024-04-19 13:19:02 +0200

    add: ex01

---
+++ ex01/format_ft_time.py
@@ -0,0 +1,9 @@
+import datetime
+
+now = datetime.datetime.now()
+
+seconds_since_epoch = now.timestamp()
+
+print(f"Seconds since January 1, 1970: {seconds_since_epoch} or {seconds_since_epoch:.2e} in scientific notation")
+
+print(now.strftime("%b %d %Y"))

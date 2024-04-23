1d444292ea4e871dd8bb42f0cedf2df8bbd6c960
Arthur Bied-Charreton <136271426+winstonallo@users.noreply.github.com>
2024-04-23 09:04:00 +0000

    add: ex02

---
+++ ex02/find_ft_type.py
@@ -0,0 +1,12 @@
+def all_thing_is_obj(object: any) -> int:
+    object_type = type(object)
+    type_strings = {
+        list: f"List : {object_type}",
+        tuple: f"Tuple : {object_type}",
+        set: f"Set : {object_type}",
+        dict: f"Dict : {object_type}",
+        str: f"{object} is in the kitchen : {object_type}",
+        int: "Type not found"
+    }
+    print(type_strings[object_type])
+    return 42

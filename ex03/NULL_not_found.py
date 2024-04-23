51d01e78723fb35b607a966af9ee7cfd88932168
Arthur Bied-Charreton <136271426+winstonallo@users.noreply.github.com>
2024-04-23 10:33:11 +0000

    add: ex03

--- ex03/NULL_not_found.py
+++ ex03/NULL_not_found.py
@@ -1,10 +1,15 @@
+import math
+
 def NULL_not_found(object: any) -> int:
     object_type = type(object)
     type_strings = {
-    None: f"Nothing: {object} {object_type}",
-    float: f"Cheese: {object} {object_type}",
-    int: f"Zero: {object} {object_type}",
-    str: f"Empty: {object}{object_type}",
-
-    bool: "Type not found"
+        float: f"Cheese: {object} {object_type}",
+        int: f"Zero: {object} {object_type}",
+        str: f"Empty: {object}{object_type}",
+        bool: f"Fake: {object} {object_type}",
     }
+    if (ret := 0 if object in [None, 0, ""] or (object_type not in [None, str] and math.isnan(object)) else 1) == 0:
+        print(type_strings[object_type] if object is not None else f"Nothing: {object} {object_type}")
+    else:
+        print("Type not found")
+    return ret

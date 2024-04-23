d79ae4a578c45c264ec2e907d8fe6be09e7cbecb
Arthur Bied-Charreton <136271426+winstonallo@users.noreply.github.com>
2024-04-23 10:36:33 +0000

    fix: math lib on ex03

--- ex03/NULL_not_found.py
+++ ex03/NULL_not_found.py
@@ -8,7 +8,8 @@ def NULL_not_found(object: any) -> int:
         str: f"Empty: {object}{object_type}",
         bool: f"Fake: {object} {object_type}",
     }
-    if (ret := 0 if object in [None, 0, ""] or (object_type not in [None, str] and math.isnan(object)) else 1) == 0:
+    nan = isinstance(object, float) and object != object
+    if (ret := 0 if object in [None, 0, ""] or nan == True else 1) == 0:
         print(type_strings[object_type] if object is not None else f"Nothing: {object} {object_type}")
     else:
         print("Type not found")
